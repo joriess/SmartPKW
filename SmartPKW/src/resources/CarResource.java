df473684e2597df7b119cfa15b5d430844642fc5
joriess <vmuser@web_dev>
2018-12-22 18:16:32 +0000

    Basic Klassen

---
+++ SmartPKW/src/resources/CarResource.java
@@ -0,0 +1,18 @@
+package resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("car")
+public class CarResource {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCar()
+	{
+		return "Hello World!";
+	}
+}
