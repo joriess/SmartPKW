9d5a5c315ebfe89d843bbf183915ec126fd25bf2
vmuser <vmuser@wstud2-0c297ff21.hof-university.de>
2019-01-05 15:33:26 +0000

    ich committe wieder. und wieder und wieder

--- SmartPKW/src/mySQLDatabase/Driver.java
+++ SmartPKW/src/mySQLDatabase/Driver.java
@@ -1,9 +1,31 @@
 package mySQLDatabase;
 
+import java.sql.*;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = "jdbc:mysql://localhost:3306/SmartPKW";
+		String username = "root";
+		String password = "";
+		System.out.println("lol");
+		try {
+			// 1. Get a connection to Databaase
+			Connection myConn = DriverManager.getConnection(url, username, password);
+			//2. Create Statement
+			Statement myStmt = myConn.createStatement();
+			
+			//3. Execute SQL query
+			ResultSet myRs = myStmt.executeQuery("select * from user");
+			
+			//4. Process result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("name"));
+				System.out.println(myRs.getString("age"));
+			}
+			
+		} catch(Exception exc) {
+			exc.printStackTrace();
+		}
 
 	}
 
